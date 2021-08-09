ab0c56c7381eb695200331fd27ff46b106305677
joso0617 <chanp617@naver.com>
2021-08-09 19:26:12 +0900

    pratice 0809

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # kasimov-git-syudy
 
-kasimov 여름방학 Git 활용 스터디 
+kasimov 여름방학 Git 활용 스터디 Repository
--- main.cpp
+++ main.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
-#include <string>
 using namespace std;
+
+class Pizza
+{
+    int radius;
+public:
+    Pizza(int  r = 0) : radius{r} {}
+    ~Pizza() {}
+    void setRadius(int r) {radius = r;}
+    void print() { cout << "Pizza(" << radius << ")" << endl; }
+};
+
+Pizza createPizza()
+{
+    Pizza p(10);
+    return p;
+}
 
 int main()
 {
-    string str;
-    cin >> str;
-    cout << str <<endl;
+    Pizza obj;
+    obj = createPizza();
+
+    obj.print();
 
     return 0;
 }
