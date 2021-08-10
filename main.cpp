e02419314adfe2b1a202f9bd020744e953dab88a
joso0617 <chanp617@naver.com>
2021-08-10 10:30:33 +0900

    08101031

--- main.cpp
+++ main.cpp
@@ -1,39 +1,42 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-class MyArray
+class Box
 {
+    double length, width, height;
 public:
-    int size;
-    int* data;
-    MyArray(int size);
-    MyArray(const MyArray& other);
-    ~MyArray();
+    Box(double l=0, double w=0, double h=0) : length{l}, width{w}, height{h} {}
+    double getVolume() {return length*width*height;}
+    friend void print(Box b);
+    Box operator+(const Box& b2)
+    {
+        Box b;
+        b.length=this->length+b2.length;
+        b.width=this->width+b2.width;
+        b.height=this->height+b2.height;
+        return b;
+    }
 };
 
-MyArray::MyArray(int size)
-{
-    this->size = size;
-    data = new int[size];
-}
-
-MyArray::MyArray(const MyArray& other)
-{
-    this->size = other.size;
-    this->data = new int[other.size];
-    for(int i=0; i<size; i++)
-        this->data[i] = other.data[i];
-}
-
-MyArray::~MyArray()
-{
-    if(data != nullptr) delete[] this->data;
-    data = nullptr;
-}
+void print(Box b)
+    {
+        cout<<"상자의 길이: "<<b.length<<endl;
+        cout<<"상자의 너비: "<<b.width<<endl;
+        cout<<"상자의 높이: "<<b.height<<endl;
+        cout<<"상자의 부피: "<<b.getVolume()<<endl;
+    }
 
 int main()
 {
-
-    
-    return 0;
+   Box a(2, 2, 2), b(3, 3, 3), c;
+   c=a+b;
+   cout<<"상자 #1"<<endl;
+   print(a);
+   cout<<"상자 #2"<<endl;
+   print(b);
+   cout<<"상자 #3"<<endl;
+   print(c);
+   
+   return 0;
 }
